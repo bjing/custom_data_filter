f0ab15f127aeca37afcc698a0c913bd2ac91c296
Brian Jing <brian.jing@au.experian.com>
2015-01-25 16:27:37 +1100

    Finalised code for submission

--- checker.py
+++ checker.py
@@ -4,7 +4,7 @@ __version__ = 0.1
 
 from config import Config
 from factories import Data_Factory, Page_Factory
-from strategy import Strategy, mass_page_match, baseline_match, magic_speedy_match
+from strategy import Strategy, baseline_match, magic_speedy_match
 
 
 def main():
@@ -23,12 +23,10 @@ def main():
     #baseline_strategy = Strategy(baseline_match)
     #baseline_strategy.find_match(data_factory, page_factory)
     
-    #mass_page_match_strategy = Strategy(mass_page_match)
-    #mass_page_match_strategy.find_match(data_factory, page_factory)
-    
     # Now we can use different strategies to find matches    
-    baseline_strategy = Strategy(magic_speedy_match)
-    baseline_strategy.find_match(data_factory, page_factory)
+    speedy_match = Strategy(magic_speedy_match)
+    speedy_match.find_match(data_factory, page_factory)
+
 
 if __name__ == '__main__':
     main()
